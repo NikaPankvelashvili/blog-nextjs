4d98053506ffac40cb9effc15f281a3d5b7f8122
NikaPankvelashvili <npanqvela@gmail.com>
2023-07-07 21:41:35 +0400

    Name update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Blog Post</title>
+      </head>
       <body className=" overflow-x-hidden">
         <Header />
         {children}
